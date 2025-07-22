02ccb101ae375ba0c1289c28faf992c573710145
Komal-2812 <sutharkomal234@gmail.com>
2025-07-22 14:58:57 +0530

    Add files via upload

---
+++ jobfit360_project/jobfit360/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='JobFit360', layout='wide')
+st.title('📄 JobFit360')
+st.markdown('Welcome to your Resume Intelligence Dashboard. Navigate using sidebar.')
